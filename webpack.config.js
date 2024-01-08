6746c58f952911a87c5c4638bb13e89b34e33411
Andrii Solovienko <osobuy@gmail.com>
2024-01-08 04:34:42 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+module.exports = {
+  entry: { yapidoo_styles: "./src/assets/styles/index.scss",
+           bootstrap: "./node_modules/bootstrap/scss/bootstrap.scss",         
+},
+  output: {
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({filename: "css/[name].css", chunkFilename: "[id].css"}),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false,
+              url: false,
+            },
+          },
+          ,
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
